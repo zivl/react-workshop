6e79803dd117e2175f193852c49f27160e952a92
Andrej Knezevski <andrej.knezevski@saplinglearning.com>
2015-08-27 14:48:55 -0400

    wip(todo-app-immutable) fixing issues from original TODO app and introducint immutability

---
+++ todo-app-immutable/app/components/todo/todo-form/todo-form.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+export default class TodoForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.saveClick = this.saveClick.bind(this);
+    this.cancelClick = this.cancelClick.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.handleChange(event);
+  }
+
+  saveClick (event) {
+    if(this.props.todo.name && this.props.todo.name.length > 0) {
+      this.props.onSave(this.props.todo);
+    } else {
+      alert('Please enter todo!!');
+    }
+  }
+
+  cancelClick (event) {
+    this.props.cancelClick();
+  }
+
+  render() {
+    return (
+      <div className='row'>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            className="form-control"
+            placeholder='Todo'
+            value={this.props.todo.name}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="col-sm-2 pull-right">
+          <button
+            className='btn btn-success btn-sm'
+            onClick={this.saveClick}>Save
+          </button>
+
+          &nbsp;&nbsp;
+
+          <button
+            className='btn btn-danger btn-sm'
+            onClick={this.cancelClick}>Cancel
+          </button>
+
+          &nbsp;&nbsp;
+
+          <button
+            className='btn btn-default btn-sm'
+            onClick={this.undoClick}>UNDO
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+TodoForm.propTypes = {
+  todo: React.PropTypes.object,
+  cancelClick: React.PropTypes.func.isRequired,
+};
---
+++ todo-app-immutable/app/components/todo/todo.js
@@ -0,0 +1,96 @@
+'use strict';
+
+import React from "react";
+import {Map, List} from "immutable";
+import TodoList from "./todo-list/todo-list";
+import TodoForm from "./todo-form/todo-form";
+
+export default class Todo extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: Map({
+        todo: Map({}),
+        todoList: List(),
+        selectedIndex: -1
+      })
+    };
+
+    this.saveTodo = this.saveTodo.bind(this);
+    this.editTodo = this.editTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.cancelClick = this.cancelClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  cancelClick() {
+    this.state.selectedIndex = -1;
+    this.state.todo = {};
+    this.setState(this.state);
+  }
+
+  saveTodo(todo) {
+    if (!todo.id && this.state.data.get('selectedIndex') === -1) {
+      this.setState((previousState) => ({
+        data: previousState.data.update('todoList', (list =>
+          list.push(todo))),
+        selectedIndex: -1
+      }));
+    } else {
+      this.setState((previousState) => ({
+        data: previousState.data.update('todoList', (list =>
+          list.set(previousState.data.selectedIndex, todo))),
+        selectedIndex: -1
+      }));
+    }
+    setTimeout(this.refs.todoForm.cancelClick);
+  }
+
+  editTodo(index) {
+    let data = this.state.data;
+
+    this.setState({
+      todo: data.get('todoList').get(index)
+    });
+  }
+
+  handleChange(event) {
+    let todoEntry = event.target.value;
+
+    this.setState((previousState) => ({
+      data: previousState.data.update('todo', (todo) =>
+        todo.set('name', todoEntry))
+    }));
+  }
+
+  deleteTodo(index) {
+    this.setState((previousState) => ({
+      data: previousState.data.update('todoList', function(todoList) {
+        return todoList.delete(index);
+      })
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <TodoForm
+          ref='todoForm'
+          todo={this.state.data.get('todo').toJS()}
+          onSave={this.saveTodo}
+          cancelClick={this.cancelClick}
+          handleChange={this.handleChange}
+        />
+        <hr/>
+        <TodoList
+          todos={this.state.data.get('todoList')}
+          editClicked={this.editTodo}
+          deleteClicked={this.deleteTodo}
+        />
+      </div>
+    );
+  }
+
+}
